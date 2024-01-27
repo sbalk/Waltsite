7a1e0092845cd6311a5707cdae6577ebe8835b80
sbalk <stijn.balk@gmail.com>
2024-01-27 16:34:19 +0100

    Updates

---
+++ _app/immutable/chunks/paths.ZU4dJoWm.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_t39790)==null?void 0:s.base)??"/sbalk";var t;const e=((t=globalThis.__sveltekit_t39790)==null?void 0:t.assets)??a;export{e as a,a as b};
--- _app/immutable/nodes/4.pjVfRj7V.js
+++
@@ -1,1 +0,0 @@
-import{s as t}from"../chunks/scheduler.nd7TcPMa.js";import{S as s,i as e}from"../chunks/index.hfpWbn6T.js";import"../chunks/entry.eOKDFlAe.js";import"../chunks/paths.Gnt7miKj.js";class p extends s{constructor(o){super(),e(this,o,null,null,t,{})}}export{p as component};
