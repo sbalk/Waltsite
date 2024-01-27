8a6f3a2178ff5811b7a4106bc284a661822a0683
sbalk <stijn.balk@gmail.com>
2024-01-27 16:29:04 +0100

    Updates

--- _app/immutable/chunks/paths.n9Wqs80d.js
+++
@@ -1,1 +0,0 @@
-var s;const a=((s=globalThis.__sveltekit_sol0lv)==null?void 0:s.base)??"/sbalk";var l;const e=((l=globalThis.__sveltekit_sol0lv)==null?void 0:l.assets)??a;export{e as a,a as b};
---
+++ _app/immutable/entry/start.GTteTIg_.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.Nj7sfiLA.js";export{t as start};
