584714cdfd3472ab70b492558154f33108decb21
sbalk <stijn.balk@gmail.com>
2024-01-29 00:04:39 +0100

    dingen

--- src/routes/DesignPage/+page.svelte
+++ src/routes/DesignPage/+page.svelte
@@ -1,10 +1,10 @@
 <script>
 	import { base } from '$app/paths';
 	// import img1 from '{base}/lib/design1.jpg';
-	import img2 from '$lib/design2.jpg';
-	import img3 from '$lib/design3.jpg';
-	import img4 from '$lib/design4.jpg';
-	import walt from '$lib/walt.png';
+	// import img2 from '$lib/design2.jpg';
+	// import img3 from '$lib/design3.jpg';
+	// import img4 from '$lib/design4.jpg';
+	// import walt from '$lib/walt.png';
 </script>
 
 <!-- <h1>Design page</h1> -->
@@ -14,29 +14,33 @@
 		<div class="text-div">
 			<div class="text-block">
 				<h1>Design</h1>
-				<p>Design is fucking awesome and if you don't pay me a ton of money for some services I'll find out where you live and stab your eyes out. Look at the following projects while you still can.</p>
+				<p>
+					Design is fucking awesome and if you don't pay me a ton of money for some services I'll
+					find out where you live and stab your eyes out. Look at the following projects while you
+					still can.
+				</p>
 			</div>
 		</div>
 	</div>
 	<div class="scroll-div">
 		<a href="{base}/DesignPage/cncptmoto">
-			<img class="image" src='{base}/design1.jpg' alt="asdf" />
+			<img class="image" src="{base}/design1.jpg" alt="asdf" />
 			<!-- <img class="image" src={img1} alt="asdf" /> -->
 		</a>
 	</div>
 	<div class="scroll-div">
 		<a href="{base}/DesignPage/cncptmoto">
-			<img class="image" src={img2} alt="asdf" />
+			<img class="image" src="{base}/design2.jpg" alt="asdf" />
 		</a>
 	</div>
 	<div class="scroll-div">
 		<a href="{base}/DesignPage/cncptmoto">
-			<img class="image" src={img3} alt="asdf" />
+			<img class="image" src="{base}/design3.jpg" alt="asdf" />
 		</a>
 	</div>
 	<div class="scroll-div">
 		<a href="{base}/DesignPage/cncptmoto">
-			<img class="image" src={img4} alt="asdf" />
+			<img class="image" src="{base}/design4.jpg" alt="asdf" />
 		</a>
 	</div>
 	<div class="scroll-div">
@@ -51,7 +55,6 @@
 	</div>
 </div>
 
-
 <style>
 	.scroll-container {
 		height: 100vh;
@@ -64,15 +67,17 @@
 		align-items: center;
 		height: 70vh;
 		scroll-snap-align: center;
-		cursor: pointer;
 	}
 	.text-div {
+		/* position: relative; */
+		/* right: 40vh; */
 		display: flex;
 		justify-content: center;
 		align-items: center;
 		height: 60vh;
+		width: 40vh;
 	}
-	.text-block{
+	.text-block {
 		display: flex;
 		flex-direction: column;
 		justify-content: center;
@@ -84,15 +89,10 @@
 		justify-content: center;
 		align-items: center;
 		height: 60vh;
-	}
-	.walt {
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		height: 50vh;
+		cursor: pointer;
 	}
 	:global(body) {
-        margin: 0;
-        padding: 0;
-    }
+		margin: 0;
+		padding: 0;
+	}
 </style>
--- svelte.config.js
+++ svelte.config.js
@@ -15,6 +15,7 @@ const config = {
 		adapter: adapter(),
 
 		paths: {
+			// base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 			base: process.env.NODE_ENV === 'production' ? '/sbalk' : ''
 		}
 	}
