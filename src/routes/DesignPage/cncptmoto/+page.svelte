25347b1e265bc3f43d12632d7073ac62584c50d5
sbalk <stijn.balk@gmail.com>
2024-01-29 00:39:35 +0100

    horizontal scroll sub project page doesn't work yet

--- src/routes/DesignPage/cncptmoto/+page.svelte
+++ src/routes/DesignPage/cncptmoto/+page.svelte
@@ -1,1 +1,54 @@
-<h1>asdf</h1>
+<script>
+	import { base } from '$app/paths';
+</script>
+
+<div class="scroll-container">
+    <!-- <div class="scroll-div">
+        <div class="scroll-div"> -->
+            <img class="image" src="{base}/sub_project1.jpg" alt="asdf" />
+        <!-- </div>
+    </div> -->
+</div>
+
+<style>
+	.scroll-container {
+		/* height: 100vh; */
+		/* scroll-snap-type: y mandatory; */
+        overflow-x: scroll;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+	.scroll-div {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: 70vh;
+		scroll-snap-align: center;
+	}
+	.text-div {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: 60vh;
+		width: 40vh;
+	}
+	.text-block {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: left;
+		height: 60vh;
+	}
+	.image {
+		/* display: flex;
+		justify-content: center;
+		align-items: center; */
+		height: 60vh;
+        /* overflow-x: scroll; */
+	}
+	:global(body) {
+		margin: 0;
+		padding: 0;
+	}
+</style>
