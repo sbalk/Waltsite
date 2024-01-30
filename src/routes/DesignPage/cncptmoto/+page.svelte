292981483167bc4cebbc02aa53261bf6da8b2907
sbalk <stijn.balk@gmail.com>
2024-01-30 23:26:21 +0100

    suddenly autoscroll at start is working after reload

--- src/routes/DesignPage/cncptmoto/+page.svelte
+++ src/routes/DesignPage/cncptmoto/+page.svelte
@@ -9,35 +9,28 @@
 		console.log(Math.round(element.scrollWidth * Math.random()));
 	});
 	const scrollToRight = async (node) => {
-		node.scroll({ left: node.scrollWidth, behavior: 'smooth' });
-		// node.scroll({ left: node.scrollWidth, behavior: 'smooth' });
+		node.scroll({ left: Math.round(element.scrollWidth * Math.random()), behavior: 'smooth' });
 	};
 
 	onMount(() => {});
 </script>
 
-<!-- poepgat horizontaal scrollen -->
-<!-- https://stackoverflow.com/questions/73987081/scroll-to-bottom-of-element-in-sveltekit -->
-<!-- https://github.com/bartholomej/svelte-scrollto-element -->
-
-
-<div bind:this={element} class="scroll-container">
-	<img class="image" src="{base}/sub_project1.jpg" alt="asdf" />
+<div class="scroll-container">
+	<div bind:this={element} class="scroll-image">
+		<img class="image" src="{base}/sub_project1.jpg" alt="asdf" />
+	</div>
 </div>
 
 <style>
 	.scroll-container {
-		/* overflow-x: scroll;
-		display: flex; */
-		/* overflow-x: scroll; */
-		width: 2700px;
-	}
-	.image {
-		width: max-content;
-		/* display: inline-flex; */
-		/* overflow: auto; */
 		top: 30vh;
 		position: absolute;
+	}
+	.scroll-image {
+		overflow: auto;
+		width: 100vw;
+	}
+	.image {
 		height: 40vh;
 	}
 	:global(body) {
--- src/routes/DesignPage/ebike/+page.svelte
+++ src/routes/DesignPage/ebike/+page.svelte
@@ -1,20 +1,40 @@
-<script>
+<script lang="ts">
 	import { base } from '$app/paths';
+	import { onMount } from 'svelte';
+	let element;
+
+	onMount(() => {
+		scrollToRight(element);
+		console.log(element.scrollWidth);
+		console.log(Math.round(element.scrollWidth * Math.random()));
+	});
+	const scrollToRight = async (node) => {
+		node.scroll({ left: Math.round(element.scrollWidth * Math.random())/10, behavior: 'smooth' });
+	};
+
+	onMount(() => {});
 </script>
 
 <div class="scroll-container">
+	<div bind:this={element} class="scroll-image">
 		<img class="image" src="{base}/Design/design-ebike-banner.jpg" alt="asdf" />
+	</div>
 </div>
 
 <style>
-	.image {
+	.scroll-container {
 		top: 30vh;
 		position: absolute;
-		height: 40vh;
-		overflow-x: scroll;
 	}
-	/* :global(body) {
+	.scroll-image {
+		overflow: auto;
+		width: 100vw;
+	}
+	.image {
+		height: 40vh;
+	}
+	:global(body) {
 		margin: 0;
 		padding: 0;
-	} */
+	}
 </style>
