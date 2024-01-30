2d8cf3c08061e902af4b42ce8d5ee55e098cb9b3
sbalk <stijn.balk@gmail.com>
2024-01-30 23:00:42 +0100

    laatste update voor vandaag

--- src/routes/DesignPage/cncptmoto/+page.svelte
+++ src/routes/DesignPage/cncptmoto/+page.svelte
@@ -3,33 +3,40 @@
 	import { onMount } from 'svelte';
 	let element;
 
-	onMount(() => scrollToRight(element));
-
+	onMount(() => {
+		scrollToRight(element);
+		console.log(element.scrollWidth);
+		console.log(Math.round(element.scrollWidth * Math.random()));
+	});
 	const scrollToRight = async (node) => {
 		node.scroll({ left: node.scrollWidth, behavior: 'smooth' });
+		// node.scroll({ left: node.scrollWidth, behavior: 'smooth' });
 	};
 
+	onMount(() => {});
 </script>
 
-<div class="scroll-container">
-	<img bind:this={element} class="image" src="{base}/sub_project1.jpg" alt="asdf" />
+<div bind:this={element} class="scroll-container">
+	<img class="image" src="{base}/sub_project1.jpg" alt="asdf" />
 </div>
 
 <style>
 	.scroll-container {
 		/* overflow-x: scroll;
 		display: flex; */
-		overflow-x: scroll;
-		width: 500px;
-		/* overflow: auto; */
+		/* overflow-x: scroll; */
+		width: 2700px;
 	}
 	.image {
+		width: max-content;
+		/* display: inline-flex; */
+		/* overflow: auto; */
 		top: 30vh;
 		position: absolute;
 		height: 40vh;
 	}
-	/* :global(body) {
+	:global(body) {
 		margin: 0;
 		padding: 0;
-	} */
+	}
 </style>
